c42c137369c264d8c8cd8518689d3fe12d364442
Kelven SIlva <kelvenkel999@gmail.com>
2024-08-20 17:36:35 -0300

    loginV2

--- js/login.js
+++ js/login.js
@@ -9,3 +9,39 @@ document.getElementById('open-login').addEventListener('click', moveOverlay)
 
 document.getElementById('open-register-mobile').addEventListener('click', moveOverlay)
 document.getElementById('open-login-mobile').addEventListener('click', moveOverlay)
+
+var input = document.querySelector('.senha');
+var input2 = document.querySelector('.senha2');
+var check = document.querySelector('.checkbox');
+var check2 = document.querySelector('.checkbox2');
+
+
+check.addEventListener('click', function () {
+  input.type = input.type == 'text' ? 'password' : 'text';
+});
+
+check2.addEventListener('click', function () {
+  input2.type = input2.type == 'text' ? 'password' : 'text';
+});
+
+
+//Precisa fazer a troca no olho
+// function mostrar(){
+
+// if(checkbox == "image/olho1.png" ){
+//     checkbox.style.img = "image/olho.png"
+// }
+// else{
+//      checkbox.style.img = "image/olho1.png"
+// }
+
+// }
+
+
+function abrir(){
+  let botao1 = document.querySelector("#open-login")
+
+  
+
+
+}
---
+++ js/move.js
@@ -0,0 +1,7 @@
+function logar(){
+    let overlay = document.getElementById("login-container")
+    let overlay2 = document.getElementById("login-container")
+
+
+    
+}
